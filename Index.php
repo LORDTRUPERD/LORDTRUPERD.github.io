cd26e32e0666019c74520d0c095800727b7eb861
BryanM <bryanmonroyleon7@gmail.com>
2025-05-19 11:27:58 -0600

    commit1
    ver 0.0.1

---
+++ Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mcfarland Toys</title>
+    <img src="Logo.jpg" alt="" class="logo-small">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Mcfarland Toys</h1>
+    <div class="container">
+        <div class="card">
+            <img src="vegeta.jpg" alt="Figura de Vegeta">
+            <h2>Vegeta Super Saiyan Blue</h2>
+            <p class="price">Q650.00</p>
+            <p class="description">Figura de acción de Vegeta en su transformación Super Saiyan Blue, con detalles precisos en el traje y expresión facial, ideal para coleccionistas y fanáticos de Dragon Ball.</p>
+            <button onclick="alert('¡Gracias por su compra!')"  class="buy-button">Comprar</button>
+        </div>
+        <div class="card">
+            <img src="goku_black.jpg" alt="Figura de Goku Black">
+            <h2>Goku Black Rose - Ultimate Atrocious</h2>
+            <p class="price">Q775.00</p>
+            <p class="description">Figura de acción de Goku Black Rose, con detalles impresionantes en su traje y efectos de energía rosa, perfecta para recrear escenas épicas de Dragon Ball Super.</p>
+            <button onclick="alert('¡Gracias por su compra!')"  class="buy-button">Comprar</button>
+        </div>
+        <div class="card">
+            <img src="Buu.jpg" alt="Figura de Buu">
+            <h2>Buu - Ultimate Atrocious</h2>
+            <p class="price">Q750.00</p>
+            <p class="description">Figura de acción de Buu en su versión Ultimate Atrocious, con acabados de alta calidad, articulaciones móviles y detalles fieles al personaje de Dragon Ball. Perfecta para coleccionistas y fanáticos de la saga.</p>
+            <button onclick="alert('¡Gracias por su compra!')" class="buy-button">Comprar</button>
+        </div>
+    </div>
+</body>
+</html>
